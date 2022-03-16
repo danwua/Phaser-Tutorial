eeb2974a932d44707f56a196163d1930291111cd
danwua <98975609+danwua@users.noreply.github.com>
2022-03-16 11:21:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = game = new Phaser.Game(config);
+
+function preload(){
+this.load.image('sky', 'assets/sky.png');
+this.load.image('ground', 'assets/ground.png');
+this.load.image('star', 'assets/star.png');
+this.load.image('bomb', 'assets/bomb.png');
+this.load.spritesheet('dude', 'assets/dude.png', {
+    frameWidth: 32, frameHeight: 48
+});
+
+}
+
+var platforms;
+
+function create(){
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+
+    player =  this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setColliderWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        repeat: -1
+    })
+}
+
+function update(){
+
+}
+
+// https://phaser.io/tutorials/making-your-first-phaser-3-game/part5
